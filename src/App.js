a73229eb73047468af4cd4ac479c556b755211ce
whit <42365284+whi-wo@users.noreply.github.com>
2019-02-21 15:04:21 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,8 +38,7 @@ class BooksApp extends React.Component {
         />
 
         <Route path="/search" render={() => (
-          <SearchPage
-          moveShelf={this.moveShelf}
+          <SearchPage books={this.state.books} moveShelf={this.moveShelf} />
           />
         )}
         />
